effe1b20ddf989dca57d667b2c2b585f7cdf7b73
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-07-29 11:56:44 -0400

    created player and token constructors

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,22 @@
-class Player {}
+// this is the player class method ... obviously
+class Player {
+  constructor(name, id, color, active = false) {
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.active = active;
+    this.tokens = this.createTokens(21);
+  }
+
+  // creates token objects for player
+  createTokens(num) {
+    const tokens = [];
+
+    for (let i = 0; i < num; i++) {
+      let token = new Token(i, this);
+      tokens.push(token);
+    }
+
+    return tokens;
+  }
+}
